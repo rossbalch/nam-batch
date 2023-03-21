0c3837ed9b66c94c06506af6e504e45bcad9a1aa
rossbalch <58158557+rossbalch@users.noreply.github.com>
2023-03-22 08:47:29 +1000

    Update export.py

--- export.py
+++ export.py
@@ -23,7 +23,6 @@ with open('list.csv') as f:
         # get time code
         time_code_dir = os.path.join('models', t)
         time_code = sorted(os.listdir(time_code_dir))[-1]
-        export_dir = os.path.join('exports', t)
 
         # construct arguments
         e = os.path.join(time_code_dir, time_code, 'lightning_logs', 'version_0', 'checkpoints')
@@ -32,20 +31,61 @@ with open('list.csv') as f:
         args = ['config_model.json', c, e]
 
         # run generate/main.py
-        subprocess.run(['python', 'generate/main.py'] + args, shell=True)
+        subprocess.run(['python', 'bin/export/main.py'] + args, shell=True)
 
         # move model.nam file to exports/
         src_file = os.path.join(e, 'model.nam')
-        dst_file = os.path.join(export_dir, 'model.nam')
-        os.makedirs(export_dir, exist_ok=True)
+        dst_file = os.path.join('exports', f'{t}.nam')
         shutil.move(src_file, dst_file)
 
         # move comparison.png and ESR.png to exports/
         src_dir = os.path.join(time_code_dir, time_code)
-        dst_dir = os.path.join(export_dir, time_code)
-        os.makedirs(dst_dir, exist_ok=True)
         for file_name in ['comparison.png', 'ESR.png']:
             src_file = os.path.join(src_dir, file_name)
-            dst_file = os.path.join(dst_dir, f'{t}_{file_name}')
+            dst_file = os.path.join('exports', f'{t}_{file_name}')
             shutil.move(src_file, dst_file)
+
 print("NAM, and Validation files generated and available in exports folder.")
+
+#########
+#########cleanup unneccessary data#########
+#########
+
+import os
+import shutil
+
+print("Cleaning up the directory structure.")
+
+folders = ["data", "models", "materials"]
+for folder_name in folders:
+    if os.path.exists(folder_name):
+        if folder_name == "materials":
+            for file_name in os.listdir(folder_name):
+                file_path = os.path.join(folder_name, file_name)
+                try:
+                    if os.path.isfile(file_path):
+                        os.unlink(file_path)
+                    elif os.path.isdir(file_path):
+                        shutil.rmtree(file_path)
+                except Exception as e:
+                    print(f"Failed to delete {file_path}. Reason: {e}")
+            print(f"Deleted all files in '{folder_name}'")
+        else:
+            shutil.rmtree(folder_name)
+            print(f"Deleted folder '{folder_name}'")
+    else:
+        print(f"Folder '{folder_name}' not found")
+
+file_names = ["list.csv", "delay.csv"]
+for file_name in file_names:
+    if os.path.exists(file_name):
+        os.remove(file_name)
+        print(f"Deleted file '{file_name}'")
+    else:
+        print(f"File '{file_name}' not found")
+
+#########
+#########finishing message#########
+#########
+
+print("All done! Have fun with your new models! Please Share. Script by Cosmic Crucible Studio.")
